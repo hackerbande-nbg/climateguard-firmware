2b65dd5d8c0e422a9065013e5701ee3e791d5ac0
seb <55889353+JustACasual@users.noreply.github.com>
2025-07-30 22:21:54 +0200

    feat: add payload version byte, skip device creation if exists

--- Heltech_Board_PIO/src/payloadFormatter.js
+++ Heltech_Board_PIO/src/payloadFormatter.js
@@ -1,13 +1,15 @@
 function decodeUplink(input) {
   var bytes = input.bytes;
 
-  var temperature = (bytes[0] << 8) | bytes[1]; // Combine MSB and LSB for temperature
-  var humidity = (bytes[2] << 8) | bytes[3];    // Combine MSB and LSB for humidity
-  var pressure = (bytes[4] << 16) | (bytes[5] << 8) | bytes[6]; // Combine MSB, middle byte, and LSB for pressure
-  var voltage = (bytes[7] << 8) | bytes[8]; // Combine MSB and LSB for voltage
+  var version = bytes[0]; // First byte is version
+  var temperature = (bytes[1] << 8) | bytes[2]; // Combine MSB and LSB for temperature
+  var humidity = (bytes[3] << 8) | bytes[4];    // Combine MSB and LSB for humidity
+  var pressure = (bytes[5] << 16) | (bytes[6] << 8) | bytes[7]; // Combine MSB, middle byte, and LSB for pressure
+  var voltage = (bytes[8] << 8) | bytes[9]; // Combine MSB and LSB for voltage
 
   return {
     data: {
+      version: version,
       temperature: temperature / 100,  // Convert to actual temperature (hundredths of degrees)
       humidity: humidity / 100,        // Convert to actual humidity (hundredths of percent)
       pressure: pressure / 100,        // Convert to actual pressure (hundredths of hPa)
