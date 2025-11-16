69d2f99d8fd4fc0f097efa5147fea881d0b91158
Jens <jens.saalmueller@gmx.de>
2025-11-16 17:48:25 +0100

    fix: mark exported functions as extern c

--- Heltech_Board_PIO/src/led.h
+++ Heltech_Board_PIO/src/led.h
@@ -1,5 +1,19 @@
+#ifndef __LED_H__
+#define __LED_H__
+
 #include <Arduino.h>
 
-void set_led(bool on);
-void toggle_led();
-void setup_led();
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void set_led(bool on);
+    void toggle_led();
+    void setup_led();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__LED_H__
